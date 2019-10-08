b721f40cb1a6c3be175326f724b94b45c3fde15b
Maxim Kravetz <mkravetz@w-r9-p5.s19.be>
2019-10-08 10:34:51 +0200

    strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkravetz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/08 10:28:48 by mkravetz          #+#    #+#             */
+/*   Updated: 2019/10/08 10:34:29 by mkravetz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	l;
+
+	i = 0;
+	l = 0;
+	while (src[l] != '\0')
+		l++;
+	if (size == 0)
+		return (l);
+	while (i + 1 < size && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	return (l);
+}
