f8a564d716395a36ba6a787e29b1a47d7e191084
Maxim Kravetz <mkravetz@w-r8-p4.s19.be>
2019-10-17 15:34:47 +0200

    atoi to change

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,15 +6,24 @@
 /*   By: mkravetz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 10:37:07 by mkravetz          #+#    #+#             */
-/*   Updated: 2019/10/15 17:07:52 by mkravetz         ###   ########.fr       */
+/*   Updated: 2019/10/15 20:39:01 by mkravetz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*
+** Faire une fonction pre check qui gere les ispace et le symb pour gagner des 
+** lignes. If pas negative, regarder si c'est min int, si pos, max int.
+** En gros commencer a partir du while str[x]>= 0 && str[x] <= 9 
+*/
+
+#include "libft.h"
+#define MIN_INT -2147483648
+#define MAX_INT 2147483647
 
 int		ft_atoi(const char *str)
 {
-	int		x;
-	int		symb;
-	int		nb;
+	int			x;
+	int			symb;
+	long		nb;
 
 	x = 0;
 	symb = 1;
