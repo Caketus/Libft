65a03a32ba6fb5ffea866216b904fcc98409ed33
Maxim Kravetz <mkravetz@w-r8-p4.s19.be>
2019-10-18 15:00:53 +0200

    strchrm strncmp et strrchr

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,22 +6,21 @@
 /*   By: mkravetz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 08:59:34 by mkravetz          #+#    #+#             */
-/*   Updated: 2019/10/10 09:28:50 by mkravetz         ###   ########.fr       */
+/*   Updated: 2019/10/17 20:29:44 by mkravetz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned	int x;
+	unsigned int	x;
+	int				res;
 
 	x = 0;
+	res = 0;
 	if (n == 0)
 		return (0);
-	while ((s1[x] != '\0') && (s2[x] != '\0') && (x < n - 1))
-	{
-		if (s1[x] != s2[x])
-			break ;
+	while ((s1[x] != '\0') && (s2[x] != '\0')
+			&& (s1[x] == s2[x]) && (n - 1 - x))
 		x++;
-	}
-	return (s1[x] - s2[x]);
+	return ((unsigned char)s1[x] - (unsigned char)s2[x]);
 }
