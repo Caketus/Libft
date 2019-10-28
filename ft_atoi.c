238ada0fd6b990791a4de757623881ac414c5d0d
Maxim Kravetz <mkravetz@e-r8-p4.s19.be>
2019-10-28 18:50:33 +0100

    putendl et putnbr, avec une correction de fonction static atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mkravetz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 10:37:07 by mkravetz          #+#    #+#             */
-/*   Updated: 2019/10/28 17:39:33 by mkravetz         ###   ########.fr       */
+/*   Updated: 2019/10/28 18:48:47 by mkravetz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #include "libft.h"
 #define MAX_SUPERLONG 9223372036854775807
 
-static int		ft_isspace_and_symb(const char *str, int *symb)
+static int	ft_isspace_and_symb(const char *str, int *symb)
 {
 	int		x;
 	long	nb;
